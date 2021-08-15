cbf04f48bd0be2436ec830cf4848078fb4ce24a9
afonso mateus <afonsoprad10@gmail.com>
2021-08-15 18:56:05 -0400

    create demonstration

--- core/src/main/java/br/com/afonsompp/App.java
+++ core/src/main/java/br/com/afonsompp/App.java
@@ -1,14 +1,24 @@
 package br.com.afonsompp;
 
+import com.google.gson.GsonBuilder;
 import br.com.afonsompp.blockchain.Blockchain;
 
 public class App {
+
 	public static void main(String[] args) {
+		var gson = new GsonBuilder().setPrettyPrinting().create();
 		Blockchain.addBlock("firstBlock");
 		Blockchain.addBlock("secondBlock");
 
+		// Demonstration of how work
 		for (var block : Blockchain.getBlockchain()) {
-			System.out.println("Hash of block: " + block.getHash());
+			System.out.println("Try mining block");
+			block.mineBlock(5);
+			System.out.println(gson.toJson(block));
+			System.out.println("Block mined");
+			System.out.println("----------------");
 		}
+
+		System.out.println(Blockchain.isChainValid());
 	}
 }
