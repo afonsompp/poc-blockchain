6e88c1040a7c37c8511a38fa742ce50817ed6a96
afonso mateus <afonsoprad10@gmail.com>
2021-08-15 15:30:11 -0400

    verifying if blockchain is valid

--- core/src/main/java/br/com/afonsompp/App.java
+++ core/src/main/java/br/com/afonsompp/App.java
@@ -1,11 +1,13 @@
 package br.com.afonsompp;
 
 import br.com.afonsompp.blockchain.Block;
+import br.com.afonsompp.blockchain.Blockchain;
 
 public class App {
 	public static void main(String[] args) {
 		Block genesisBlock = new Block("0", "First block");
-
-		System.out.println(genesisBlock.getHash());
+		Blockchain.addBlock(genesisBlock);
+		Blockchain.addBlock(new Block(genesisBlock.getHash(), "Second block"));
+		System.out.println(Blockchain.isChainValid());
 	}
 }
--- core/src/main/java/br/com/afonsompp/blockchain/Blockchain.java
+++ core/src/main/java/br/com/afonsompp/blockchain/Blockchain.java
@@ -9,4 +9,23 @@ public class Blockchain {
 	public static void addBlock(Block block) {
 		blockchain.add(block);
 	}
+
+	public static Boolean isChainValid() {
+		Block currentBlock;
+		Block previousBlock;
+
+		if (blockchain.size() <= 1)
+			return true;
+
+		for (int i = 1; i < blockchain.size(); i++) {
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i - 1);
+
+			if (!currentBlock.getPreviusHash().equals(previousBlock.getHash()))
+				return false;
+
+		}
+
+		return true;
+	}
 }
