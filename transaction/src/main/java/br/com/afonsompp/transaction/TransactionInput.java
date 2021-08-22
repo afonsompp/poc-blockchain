15a101afb389631ac11363b2abfb5615f031be28
afonso mateus <afonsoprad10@gmail.com>
2021-08-22 13:09:53 -0400

    define TransactionInput

--- transaction/src/main/java/br/com/afonsompp/transaction/TransactionInput.java
+++ transaction/src/main/java/br/com/afonsompp/transaction/TransactionInput.java
@@ -2,4 +2,22 @@ package br.com.afonsompp.transaction;
 
 public class TransactionInput {
 
+	private String transactionOutputId;
+	private TransactionOutput UTXO;
+
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
+
+	public String getTransactionOutputId() {
+		return transactionOutputId;
+	}
+
+	public TransactionOutput getUTXO() {
+		return UTXO;
+	}
+
+	public void setUTXO(TransactionOutput UTXO) {
+		this.UTXO = UTXO;
+	}
 }
