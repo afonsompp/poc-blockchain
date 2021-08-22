b3d44a0332e207778cf7d7f2c09ced77780e6562
afonso mateus <afonsoprad10@gmail.com>
2021-08-22 12:19:14 -0400

    test signature of transactions

--- transaction/src/main/java/br/com/afonsompp/App.java
+++ transaction/src/main/java/br/com/afonsompp/App.java
@@ -1,5 +1,29 @@
 package br.com.afonsompp;
 
+import br.com.afonsompp.blockchain.utils.StringUtils;
+import br.com.afonsompp.transaction.Transaction;
+import br.com.afonsompp.transaction.Wallet;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Security;
+
 public class App {
-	public static void main(String[] args) {}
+
+	public static void main(String[] args) {
+		Security.addProvider(new BouncyCastleProvider());
+
+		var walletA = new Wallet();
+		var walletB = new Wallet();
+
+		System.out.println("Private and public keys:");
+		System.out.println(StringUtils.getStringFromKey(walletA.getPublicKey()));
+		System.out.println(StringUtils.getStringFromKey(walletA.getPrivateKey()));
+
+		Transaction transaction = new Transaction(walletA.getPublicKey(), walletB.getPublicKey(), 5D,
+			null);
+		transaction.generateSignature(walletA.getPrivateKey());
+
+		System.out.println("Is signature verified");
+		System.out.println(transaction.verifySignature());
+	}
 }
