cb45336ad46eee582bbfdf4dbd5eb03547c37c41
afonso mateus <afonsoprad10@gmail.com>
2021-08-22 17:12:40 -0400

    add blockchain test

--- core/src/main/java/br/com/afonsompp/App.java
+++ core/src/main/java/br/com/afonsompp/App.java
@@ -1,24 +1,44 @@
 package br.com.afonsompp;
 
 import br.com.afonsompp.blockchain.Blockchain;
-import br.com.afonsompp.blockchain.utils.StringUtils;
+import br.com.afonsompp.transaction.Transaction;
+import br.com.afonsompp.transaction.TransactionOutput;
+import br.com.afonsompp.transaction.Transactions;
+import br.com.afonsompp.transaction.Wallet;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Security;
 
 public class App {
 
 	public static void main(String[] args) {
+		Security.addProvider(new BouncyCastleProvider());
 
-		Blockchain.addBlock("firstBlock");
-		Blockchain.addBlock("secondBlock");
+		var walletA = new Wallet();
+		var walletB = new Wallet();
+		var coinbase = new Wallet();
 
-		// Demonstration of how work
-		for (var block : Blockchain.getBlockchain()) {
-			System.out.println("Try mining block");
-			block.mineBlock(5);
-			System.out.println(StringUtils.toJsonString(block));
-			System.out.println("Block mined");
-			System.out.println("----------------");
-		}
+		var genTransac = new Transaction(coinbase.getPublicKey(), walletA.getPublicKey(), 100d, null);
+		genTransac.setId("0");
+		genTransac.generateSignature(coinbase.getPrivateKey());
+		genTransac.getOutputs()
+			.add(new TransactionOutput(genTransac.getReciepient(), genTransac.getValue(), genTransac.getId()));
+		Transactions.getUTXOs().put(genTransac.getOutputs().get(0).getId(), genTransac.getOutputs().get(0));
 
-		System.out.println(Blockchain.isChainValid());
+		System.out.println("Creating and Mining Genesis block... ");
+		Blockchain.addBlock(genTransac);
+
+		Blockchain.addBlock(walletA.sendFunds(walletB.getPublicKey(), 40d));
+		System.out.println("\nWalletA's balance is: " + walletA.getBalance());
+		System.out.println("WalletB's balance is: " + walletB.getBalance());
+
+		Blockchain.addBlock(walletA.sendFunds(walletA.getPublicKey(), 20d));
+		System.out.println("\nWalletA's balance is: " + walletA.getBalance());
+		System.out.println("WalletB's balance is: " + walletB.getBalance());
+
+		Blockchain.addBlock(walletA.sendFunds(walletA.getPublicKey(), 2d));
+		System.out.println("\nWalletA's balance is: " + walletA.getBalance());
+		System.out.println("WalletB's balance is: " + walletB.getBalance());
+
 	}
 }
