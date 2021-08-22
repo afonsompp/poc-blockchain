23acd8a762b71a50bb6d2e443383b0671acf7d9a
afonso mateus <afonsoprad10@gmail.com>
2021-08-22 11:51:13 -0400

    generate and verify signature

--- utils/src/main/java/br/com/afonsompp/blockchain/utils/StringUtils.java
+++ utils/src/main/java/br/com/afonsompp/blockchain/utils/StringUtils.java
@@ -5,6 +5,9 @@ import com.google.gson.GsonBuilder;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
 import java.util.Base64;
 
 public class StringUtils {
@@ -24,4 +27,26 @@ public class StringUtils {
 		return gson.toJson(object);
 	}
 
+	public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+		try {
+			var dsa = Signature.getInstance("ECDSA", "BC");
+			dsa.initSign(privateKey);
+			dsa.update(input.getBytes());
+			return dsa.sign();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+		try {
+			var ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+			ecdsaVerify.initVerify(publicKey);
+			ecdsaVerify.update(data.getBytes());
+			return ecdsaVerify.verify(signature);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
 }
